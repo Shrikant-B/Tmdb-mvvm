0784bc531453f27e543f6c4cf42a63dd4f1ddc4c
Shrikant Badwaik <shrikant.badwaik@gmail.com>
2018-10-12 14:29:11 +0530

    project files removed from master branch

--- app/src/main/java/com/shrikantbadwaik/tmdb/data/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.shrikantbadwaik.tmdb.data.repository;
-
-import com.shrikantbadwaik.tmdb.data.model.MovieResponse;
-
-import io.reactivex.Observable;
-
-public interface Repository {
-    Observable<MovieResponse> upcomingMovies();
-}
--- app/src/main/java/com/shrikantbadwaik/tmdb/domain/helper/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.shrikantbadwaik.tmdb.domain.helper;
-
-public final class Constants {
-    public static final boolean DEBUG = true;
-    public static final String URL = "https://api.themoviedb.org/3/";
-    public static final String API_KEY = "b7cd3340a794e5a2f35e3abb820b497f";
-    public static final String API_ERROR_RESPONSE_KEY = "";
-}
